3229322b776a3cd997d583aabbfb4f9f58ff836e
javoperez <javierjorge77@gmail.com>
2014-03-25 19:10:21 -0600

    add paypal2

--- subetechv1/settings.py
+++ subetechv1/settings.py
@@ -109,7 +109,7 @@ if DEBUG:
     print STATIC_ROOT, "static root"
 #######################################################
 #           HEROKU CONFIGURATION: 
-"""
+
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
@@ -117,4 +117,3 @@ DATABASES['default'] =  dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
-"""
